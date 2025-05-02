7c757065c85b160fffc3c42c548fbfb1c826e403
Arya Pratap Singh <notaryasingh@gmail.com>
2025-05-02 16:07:23 +0530

    feat: structural implementation
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,33 +12,45 @@ import { Button } from "./ui/button";
 import ReddishLogo from "@/images/Reddish Full.png";
 import ReddishLogoOnly from "@/images/Reddish Logo Only.png";
 import Image from "next/image";
-import { MenuIcon } from "lucide-react";
+import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { useSidebar } from "./ui/sidebar";
 
 function Header() {
   const { user } = useUser();
+  const { toggleSidebar, open, isMobile } = useSidebar();
+  const isBanned = user?.publicMetadata["IS_BANNED"] as boolean;
+
   return (
-    <header className="
+    <header
+      className="
     flex items-center justify-between
     p-4 border-b border-gray-200
-    ">
+    "
+    >
       {/* Left */}
 
-      <div className="flex items-center gap-2">
-        <MenuIcon className="w-6 h-6"/>
-        <Image
-          src={ReddishLogo}
-          alt="logo"
-          width={150}
-          height={150}
-          className="hidden md:block"
-        />
-        <Image
-          src={ReddishLogoOnly}
-          alt="logo"
-          width={40}
-          height={40}
-          className="block md:hidden"
-        />
+      <div className="flex items-center h-10">
+        {open && !isMobile ? (
+          <ChevronLeftIcon className="w-6 h-6" onClick={toggleSidebar} />
+        ) : (
+          <div className="flex items-center gap-2">
+            <MenuIcon className="w-6 h-6" onClick={toggleSidebar} />
+            <Image
+              src={ReddishLogo}
+              alt="logo"
+              width={150}
+              height={150}
+              className="hidden md:block"
+            />
+            <Image
+              src={ReddishLogoOnly}
+              alt="logo"
+              width={40}
+              height={40}
+              className="block md:hidden"
+            />
+          </div>
+        )}
       </div>
 
       <div>
